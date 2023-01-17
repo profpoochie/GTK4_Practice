69a1409cdd5fe9583cae1b4845802cb8676994b2
Marvin Borhillo <homepoochie@gmail.com>
2023-01-18 00:49:01 +0800

    Memory management

--- practice_doc_01/src/main.rs
+++ practice_doc_01/src/main.rs
@@ -1,6 +1,11 @@
+use std::cell::Cell;
+use std::rc::Rc;
+
+use glib::clone;
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Button};
-const APP_ID: &str = "org.gtk_rs.HelloWorld3";
+use gtk::{self, glib, Application, ApplicationWindow, Button, Orientation};
+
+const APP_ID: &str = "org.gtk_rs.GObjectMemoryManagement3";
 
 fn main() {
     // Create a new application
@@ -14,30 +19,51 @@ fn main() {
 }
 
 fn build_ui(app: &Application) {
-    // Create a button with label and margins
-    let button = Button::builder()
-        .label("Press me!")
+    // Create two buttons
+    let button_increase = Button::builder()
+        .label("Increase")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+    let button_decrease = Button::builder()
+        .label("Decrease")
         .margin_top(12)
         .margin_bottom(12)
         .margin_start(12)
         .margin_end(12)
         .build();
 
-    // Connect to "clicked" signal of `button`
-    button.connect_clicked(|button| {
-        // Set the label to "Hello World!" after the button has been clicked on
-        button.set_label("Hello World!");
-    });
+    let number = Rc::new(Cell::new(0));
+
+    // Connect callbacks
+    // When a button is clicked, `number` and label of the other button will be changed
+    button_increase.connect_clicked(clone!(@weak number, @strong button_decrease =>
+        move |_| {
+            number.set(number.get() + 1);
+            button_decrease.set_label(&number.get().to_string());
+    }));
+    button_decrease.connect_clicked(clone!(@strong button_increase =>
+        move |_| {
+            number.set(number.get() - 1);
+            button_increase.set_label(&number.get().to_string());
+    }));
+
+    // Add buttons to `gtk_box`
+    let gtk_box = gtk::Box::builder()
+        .orientation(Orientation::Vertical)
+        .build();
+    gtk_box.append(&button_increase);
+    gtk_box.append(&button_decrease);
 
     // Create a window
     let window = ApplicationWindow::builder()
         .application(app)
         .title("My GTK App")
-        .child(&button)
-        .default_height(300)
-        .default_width(300)
+        .child(&gtk_box)
         .build();
 
-    // Present window
+    // Present the window
     window.present();
 }
