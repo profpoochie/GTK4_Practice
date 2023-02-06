50b8bc21dcf3ab5677892bf2e379b9d7cf6b398a
Marvin Borhillo <homepoochie@gmail.com>
2023-02-06 10:19:00 +0800

    try YAML on rust

---
+++ ingest_yaml/src/main.rs
@@ -0,0 +1,23 @@
+use std::fs::File;
+use std::io::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Person {
+    name: String,
+    age: u8,
+    addresses: Vec<String>,
+}
+
+fn main() {
+    let mut file = File::open("person.yaml").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let p: Person = serde_yaml::from_str(&contents).unwrap();
+
+    // output intended data
+    println!("{:#?}", p.name);
+    println!("{:#?}", p.age);
+    println!("{:#?}", p.addresses);
+}
