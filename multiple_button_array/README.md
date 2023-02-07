75672255c055cb3bfe55736602337f7ea39792c5
Marvin Borhillo <homepoochie@gmail.com>
2023-02-07 17:59:29 +0800

    Version that generate buttons  with functions in YAML file.

---
+++ multiple_button_array/README.md
@@ -0,0 +1,2 @@
+This app can generate buttons (arbitrary number) with function on YAML config file.
+![Screenshot](generate_buttons.png)
--- multiple_button_array/src/main.rs
+++ multiple_button_array/src/main.rs
@@ -1,13 +1,15 @@
 use gtk::prelude::*;
 use gtk::{Application, ApplicationWindow, Box, Button};
-//use core::result::IntoIter;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::Read;
+use std::process::Command;
 
+// Struct for YAML config
 #[derive(Serialize, Deserialize, Debug)]
 struct IngestCom {
     buttons: Vec<String>,
+    commands: Vec<String>,
 }
 
 fn main() {
@@ -28,35 +30,49 @@ fn build_ui(application: &gtk::Application) {
 
     let window = ApplicationWindow::builder()
         .application(application)
-        .title("Generate Buttons Example")
+        .title("Generate Buttons")
         .default_width(300)
         .default_height(300)
         .build();
     let names = inc.buttons;
-
-    // setting buttons based on YAML config file.
-    let buttons: Vec<Button> = names
-        .into_iter()
-        .map(|name| {
-            let button = Button::with_label(&name);
-            button.connect_clicked(move |_| {
-                println!("Button clicked");
-            });
-            button
-        })
-        .collect();
+    let actions = inc.commands;
 
     let container = Box::builder()
         .halign(gtk::Align::Center)
         .orientation(gtk::Orientation::Vertical)
+        .margin_top(24)
+        .margin_bottom(24)
         .spacing(24)
         .build();
-    for button in buttons {
-        container.append(&button);
+
+    // setting buttons based on YAML config file.
+    for (names, actions) in names.iter().zip(actions.iter()){
+        let buttons = Button::with_label(names);
+        let actions = actions.clone();
+        buttons.connect_clicked(move |_|{
+            term_command(actions.to_string());
+        });
+        container.append(&buttons);
     }
+
+
     window.set_child(Some(&container));
     // window.show_all();
     // application.run(&[]);
     window.show();
+}
+
+// Terminal command
+fn term_command(input_string:String) {
+    let input_vec: Vec<&str> = input_string.split(" ").collect();
+    let command = input_vec[0];
+    let args = &input_vec[1..];
+    let output = Command::new(command)
+        .args(args)
+        .output()
+        .expect("Failed to run command");
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    println!("{}", stdout);
 }
 
